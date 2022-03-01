37a6c8c3d733013d3bfcc6b5af3c8b243581b6a8
20jun <ex_xe@naver.com>
2022-03-01 13:28:57 +0900

    #6.4 Cleanup Func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div className="App">
-      <h1>Welcome back!</h1>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
