a479d5dd93094da16b3bd9f71951e5deb5dabbab
20jun <ex_xe@naver.com>
2022-03-01 16:09:16 +0900

    #7.2 challenge finish

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState(0);
+  const [selected, setSelected] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+        setSelected(json[0].quotes.USD.price);
+      });
+  }, []);
 
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setMyMoney(e.target.value);
   };
-  console.log(toDos);
-
+  console.log(selected);
+  const onSelect = (e) => {
+    console.log(e);
+    setSelected(e.target.value);
+  };
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <input
+        onChange={onChange}
+        value={myMoney}
+        type="number"
+        placeholder="money"
+      />
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect} value={selected}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      )}
+      <h3>by BTC {myMoney / selected}</h3>
     </div>
   );
 }
