491228e9c914767a4da83da331a430cf31d20e7b
20jun <ex_xe@naver.com>
2022-03-02 23:52:05 +0900

    #7.3

--- src/App.js
+++ src/App.js
@@ -2,51 +2,16 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [myMoney, setMyMoney] = useState(0);
-  const [selected, setSelected] = useState(0);
+
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
       .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-        setSelected(json[0].quotes.USD.price);
-      });
+      .then((json) => console.log(json));
   }, []);
 
-  const onChange = (e) => {
-    console.log(e.target.value);
-    setMyMoney(e.target.value);
-  };
-  console.log(selected);
-  const onSelect = (e) => {
-    console.log(e);
-    setSelected(e.target.value);
-  };
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input
-        onChange={onChange}
-        value={myMoney}
-        type="number"
-        placeholder="money"
-      />
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={onSelect} value={selected}>
-          {coins.map((coin) => (
-            <option key={coin.id} value={coin.quotes.USD.price}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-      <h3>by BTC {myMoney / selected}</h3>
-    </div>
-  );
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
