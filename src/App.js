bcf20351fd0fdcfc7afb9bd326327da22bd0292a
20jun <ex_xe@naver.com>
2022-03-09 23:38:15 +0900

    DNF API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const getDF = async (text) => {
+    console.log(text);
+    const json = await (
+      await fetch(
+        `https://cors-anywhere.herokuapp.com/https://api.neople.co.kr/df/servers/anton/characters?characterName=${text}&apikey=3ZJOnzvz1XN7513uG4IGBKZzqRRKoa2s`
+      )
+    ).json();
+
+    console.log(json.rows[0]);
+    setName(json.rows[0].jobGrowName);
+  };
+  // useEffect(() => {
+  //   getDF();
+  // }, []);
+
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  const onClick = (e) => {
+    console.log(e);
+    getDF(text);
+  };
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <input type="text" value={text} onChange={onChange} />
+      <button onClick={onClick}>검색</button>
+      <h1>{name}</h1>
+    </>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/movie/:id">
+    //       <Detail />
+    //     </Route>
+    //     <Route path="/">
+    //       <Home />
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 }
 
